bba7aa2cae572a01deed9a01ecf112bd2b0134af
avafox <avafoxemail@gmail.com>
2024-04-11 16:03:32 -0400

    Initialize bricks for breakout and introduce beginning logic

--- breakout.js
+++ breakout.js
@@ -1,9 +1,78 @@
 document.addEventListener("keydown", onKeyDown);
 document.addEventListener("keyup", onKeyUp);
+// document.body.onload = createBricks;
 
 let leftPressed = false;
 let rightPressed = false;
 let paddleSpeed = 3;
+const brickColumn = 3;
+const brickRow = 3; 
+let bricks = [];
+
+function updateBricks () {
+    // Remove all bricks and create again based on True/False in bricks
+}
+
+function initBricks() {
+    /* create a bunch of divs w/ their lil margins separated
+    from each other in big square
+    Array of arrays, each element boolean 
+    */
+   for (let i = 0; i < brickRow; i++) {
+        let newRow = []
+        for (let j = 0; j < brickColumn; j++) {
+            if (i == 0 && j == 0) {
+                newRow.push(false);
+            }
+            else {
+                newRow.push(true);
+            } 
+        }
+        bricks.push(newRow);
+   }
+}
+initBricks();
+
+function createBricks() {
+    // createElement innerHTML
+    // getting brickhouse, creating bricks, appending bricks to house
+    let house = document.getElementById("brickHouse");
+    let currentX = 0;
+    let currentY = 0;
+    let brickWidth;
+    let brickHeight;
+    let margin = 4;
+    // create brick, get its shit, remove
+    let fakeBrick = document.createElement("div");
+    house.appendChild(fakeBrick);
+    fakeBrick.setAttribute("class", "brick");
+    let rect = fakeBrick.getBoundingClientRect();
+    brickWidth = rect.width;
+    brickHeight = rect.height;
+    house.removeChild(fakeBrick);
+
+    for (let i = 0; i < brickRow; i++) {
+        for (let j = 0; j < brickColumn; j++) {
+            if (bricks[i][j]) {
+            let newBrick = document.createElement("div");
+            // GIving newbrick class "brick"
+            house.appendChild(newBrick);
+            newBrick.setAttribute("class", "brick");
+            newBrick.style.left = `${currentX}px`;
+            newBrick.style.top = `${currentY}px`;
+            
+            console.log(`Current X: ${currentX}`);
+            console.log(`Current Y: ${currentY}`);
+            console.log(brickWidth);
+            console.log(brickHeight);
+            }
+            currentX += brickWidth + margin;
+        }
+        currentX = 0;
+        currentY += brickHeight + margin;
+    }
+}
+createBricks()
 
 function game() {
     movePaddle();
@@ -44,8 +113,57 @@ function movePaddle() {
     }
 }
 
-function createBricks() {
-    /* create a bunch of divs w/ their lil margins separated
-    from each other in big square
-    */
+function moveBall() {
+    let ball = document.getElementById("ball");
+    let ballPos = ball.getBoundingClientRect();
+    let topBall = ballPos.top;
+    let bottomBall = ballPos.bottom;
+    let leftBall = ballPos.left;
+    let rightBall = ballPos.right;
+    
+    // move down
+    if (movingDown) {
+        if (bottomBall <= window.innerHeight) {
+            ball.style.top = `${topBall + ballSpeed}px`
+        }
+        else {
+            movingDown = false;
+            movingUp = true;
+        }
+    }
+    else if (movingUp) {
+        if (topBall >= 0) {
+            ball.style.top = `${topBall - ballSpeed}px`
+        }
+        else {
+            movingUp = false;
+            movingDown = true;
+        }
+    }
+    if (moveLeft) {
+        if (leftBall >= 0) {
+            ball.style.left = `${leftBall - ballSpeed}px`
+        }
+        // Hit Left wall
+        else {
+            moveLeft = false;
+            moveRight = true;
+            scoreRight++;
+            scoreRightEl.innerHTML = scoreRight;
+        }
+    }
+    else if (moveRight) {
+        if (rightBall <= window.innerWidth) {
+            ball.style.left = `${leftBall + ballSpeed}px`
+        }
+        // Hit right wall
+        else {
+            moveRight = false;
+            moveLeft = true;
+            scoreLeft++;
+            scoreLeftEl.innerHTML = scoreLeft;  
+        }
+    }
+    return ballPos;
 }
+
